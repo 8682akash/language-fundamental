47bb7464a0d17f2c3efa8598aa00d281b8fab283
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-21 10:23:12 +0530

    Add project files.

---
+++ ass2009(1)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass2009_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the marks of physics");
+            int phy=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the marks of chemistry");
+            int chem=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the marks of maths");
+            int math=Convert.ToInt32(Console.ReadLine());
+            int total = phy+chem+math;
+            Console.WriteLine("the marks of akash is" + total);
+            double per = total * 100f / 300;
+            Console.WriteLine("the percentage of akash is" + per);
+            if (per < 100 && per >80)
+            {
+                Console.WriteLine("Grade A");
+            }
+            else if(per<80 && per>70)
+            {
+                Console.WriteLine("Grade B");
+            }
+            else if(per<70 && per > 60)
+            {
+                Console.WriteLine("Grade C");
+                
+            }
+            else if (per<60 && per > 35)
+            {
+                Console.WriteLine("Grade D");
+            }
+          
+        }
+    }
+}
---
+++ ass2009(2)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass2009_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the BS");
+            int BS=Convert.ToInt32(Console.ReadLine());
+            if( BS <= 10000)
+            {
+                int DA = (BS * 80) / 100;
+                int HRA = (BS * 20) / 100;
+                int GS = BS + DA + HRA;
+                Console.WriteLine("gross sallary is " + GS);
+            }else if (BS <= 20000)
+            {
+                int DA1 = (BS * 90) / 100;
+                int HRA1 = (BS * 35) / 100;
+                int GS1 = (BS + DA1) + HRA1;
+                Console.WriteLine("gross sallary is " + GS1);
+            }
+            else
+            {
+                int DA2 = (BS * 95) / 100;
+                int HRA2 = (BS * 40) / 100;
+                int GS2 = (BS + DA2) + HRA2;
+                Console.WriteLine("gross sallary is " + GS2);
+            }
+        }
+    }
+}
