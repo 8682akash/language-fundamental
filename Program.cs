7c4ac13d4ee95daaaa0313c7a670ef7a1fc867a1
8682akash <99233617+8682akash@users.noreply.github.com>
2022-09-22 18:54:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ak2109
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ----  WAP to print numbers from 100 -  50  -------
+
+
+            for (int i = 100; i >= 50; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
